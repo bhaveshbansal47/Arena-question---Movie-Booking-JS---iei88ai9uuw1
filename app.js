14cd3ea04e52ea2d04d66a960d8bc80a3cde648a
bhavesh <32275448+bhaveshbansal47@users.noreply.github.com>
2023-11-14 23:10:39 +0530

    feat: major project

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+import { fetchMovieAvailability, fetchMovieList } from "./api.js"
+const loader = document.getElementById('loader')
+const mainNode = document.getElementsByTagName('main')[0]
+
+fetchMovieList().then(function (movies) {
+    loader.remove()
+    createMovieCardsUi(movies)
+})
+
+function createMovieCardsUi(movies) {
+    const movieContainerNode = document.createElement('div')
+    movieContainerNode.setAttribute('class', 'movie-holder')
+    let movieCardsHtml = ''
+    for (const movie of movies) {
+        movieCardsHtml += `<a class="movie-link" href="/${movie.name}">
+        <div class="movie" data-d="${movie.name}">
+        <div class="movie-img-wrapper" style="background-image: url('${movie.imgUrl}'); background-size: cover;"> 
+        </div>
+        <h4>${movie.name}</h4>
+        </div>
+        </a>
+       `
+    }
+
+    movieContainerNode.innerHTML = movieCardsHtml
+    mainNode.appendChild(movieContainerNode)
+}
